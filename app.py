e796787ec033c95b673c92e332f4e17b6654e851
Solofox9 <tddinesh2005@gmail.com>
2025-10-17 12:04:16 +0530

    Fix Flask app initialization syntax error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__render_template__)
 
 @app.route("/")
 def welcome():
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     # Default to port 5000 if PORT is not set in the environment
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
